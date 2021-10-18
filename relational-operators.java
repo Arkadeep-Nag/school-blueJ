e1cda0e708c3879c2a2858f333849a33b5c9c210
Arkadeep Nag <66512429+Arkadeep-Nag@users.noreply.github.com>
2021-10-18 21:24:44 +0530

    Create relational-operators.java

---
+++ relational-operators.java
@@ -0,0 +1,25 @@
+/**
+  @author Arkadeep Nag
+ */
+public class Arkadeep
+{
+    public static void main(){
+        //Simple Variables with Integer Value
+        int x,y,z;
+        x=10;y=20;
+        if(x>y){
+            System.out.println("x is greater than y");
+            
+        }
+        else if (x<y) {
+            System.out.println("y is greater than x");
+            
+        }
+        else {
+            System.out.println("x is equal to y");
+            
+        }
+        
+    
+    }
+}
