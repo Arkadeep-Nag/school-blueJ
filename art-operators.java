b034fa458c1a9b810b00d198dd3467cc5c3761da
Arkadeep Nag <66512429+Arkadeep-Nag@users.noreply.github.com>
2021-10-18 21:07:36 +0530

    Create art-operators.java

---
+++ art-operators.java
@@ -0,0 +1,24 @@
+
+/**
+  @author Arkadeep Nag
+ */
+public class Arkadeep
+{
+    public static void main(){
+        //Simple Variables with Integer Value
+        int x,y,z;
+        x = 300;
+        y=291;
+        z =1234;
+        //Addition with "+" Operator 
+        System.out.println(x + y + z);
+        //Substraction with "-" Operator
+        System.out.println(z-x);
+        //Multiplication of x and y with "*" Operator
+        System.out.println(x*y);
+        
+        
+        
+    
+    }
+}
